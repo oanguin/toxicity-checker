8411ba0a90d011103ffadcf4bd459795898e3a66
Oneal Anguin <oneal.anguin@ef.com>
2022-04-27 22:24:59 +0100

    Update name of main class and add OpenData plugin closes #2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,13 +5,12 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:toxicity_checker/main.dart';
 
 void main() {
   testWidgets('Toxicity Checker smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const ToxicityChecker());
 
     // Toxicity Checker should be in the header and maybe elsewhere
     expect(find.text('Toxicity Checker'), findsWidgets);
