789c9c079bf459ca5597076b1ed351f45733a456
Oneal Anguin <oneal.anguin@ef.com>
2022-05-01 16:30:01 +0100

    Add text box for barcodes; closes #3

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,15 +4,29 @@
 // utility that Flutter provides. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:toxicity_checker/main.dart';
 
 void main() {
-  testWidgets('Toxicity Checker smoke test', (WidgetTester tester) async {
+  testWidgets('TEST: Check if Toxicity Checker is Present',
+      (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const ToxicityChecker());
 
     // Toxicity Checker should be in the header and maybe elsewhere
     expect(find.text('Toxicity Checker'), findsWidgets);
+  });
+
+  testWidgets('TEST: Check that Barcode Input Is present',
+      (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const ToxicityChecker());
+
+    // I should be able to enter text into text field with key `_txtBarCode`
+    await tester.enterText(
+        find.byKey(const Key("_txtBarCode")), 'Hello New Barcode');
+
+    expect(find.text('Hello New Barcode'), findsWidgets);
   });
 }
